603c51c6b062389045397295668eaa8734b2697d
GeunhoYu <ghyu9384@gamil.com>
2023-06-21 00:22:41 +0900

    0621

---
+++ BOJ/go.py
@@ -0,0 +1,20 @@
+import math
+
+N = int(input())
+n = int(math.sqrt(N))
+E = [i for i in range(n, 0, -1)]
+size = len(E)
+ans = 5
+
+
+def go(start, cnt, rlt):
+    global ans
+    if rlt == N:
+        ans = min(ans, cnt)
+    if cnt == 4:
+        return
+    for i in range(start, size):
+        go(i+1, cnt+1, rlt+E[i]**2)
+
+go(0, 0, 0)
+print(ans)
